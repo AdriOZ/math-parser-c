771c4edd0d20f628249e88b456c12ec456cae4d9
AdriOZ <adrianozamora@gmail.com>
2021-05-08 16:46:13 +0200

    Script to build the program for debugging

---
+++ debug.sh
@@ -0,0 +1,11 @@
+gcc                 \
+    token.c         \
+    tokenizer.c     \
+    token_list.c    \
+    parser.c        \
+    main.c          \
+-lm                 \
+-std=c18            \
+-g                  \
+-o                  \
+    main.o
